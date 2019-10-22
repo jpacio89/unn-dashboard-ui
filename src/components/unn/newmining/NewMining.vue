3ccecac62b5faace02923891346b34374a8de2f0
João Coelho <admin.henyah@gmail.com>
2019-10-22 22:16:59 +0200

    no message

--- src/components/unn/newmining/NewMining.vue
+++ src/components/unn/newmining/NewMining.vue
@@ -9,10 +9,23 @@
         </va-card>
       </div>
       <div class="flex xs12 lg6">
-        <va-card title="Features">
-          1 <br/>
-          2
-        </va-card>
+        <va-list fit class="mb-2">
+          <va-item v-for="n in 10">
+            <va-item-section side>
+              <va-checkbox v-model="appBanners" />
+            </va-item-section>
+            <va-item-section>
+              <va-item-label>
+                <h1 style="text-transform: capitalize;">feathers</h1>
+              </va-item-label>
+
+              <va-item-label caption style="margin-top: 5px;">
+                <va-badge class="mb-2" color="dark" outline>true</va-badge>
+                <va-badge class="mb-2" color="dark" outline>false</va-badge>
+              </va-item-label>
+            </va-item-section>
+          </va-item>
+        </va-list>
       </div>
     </div>
   </div>
